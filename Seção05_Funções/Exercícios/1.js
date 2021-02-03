e76ca4ace47205336a62cba0b4f25a25254f793a
Lucas Mendes <57737521+lucasmendeslms@users.noreply.github.com>
2021-02-02 23:31:46 -0300

    Primeiro exercício - Lista de Funções

--- Seção05_Funções/Exercícios/1.js
+++ Seção05_Funções/Exercícios/1.js
@@ -1,1 +1,19 @@
-function
+const operacoes = function (n1, n2) {
+    console.log('Soma: ' + (n1 + n2)
+        + '\nSubtração: ' + (n1 - n2)
+        + '\nDivisão: ' + (n1 / n2)
+        + '\nMultiplicação: ' + (n1 * n2))
+}
+
+operacoes(5, 2)
+
+//Arrow
+console.log('_____________________\n')
+ 
+const soma = (num1, num2) => {
+    return console.log('Soma: ' + (num1 + num2)
+        + '\nSubtração: ' + (num1 - num2)
+        + '\nDivisão: ' + (num1 / num2)
+        + '\nMultiplicação: ' + (num1 * num2))
+}
+soma(1, 2)
