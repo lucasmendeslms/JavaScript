2028abfe40864975149b34550c25d24ffb048266
Lucas Mendes <57737521+lucasmendeslms@users.noreply.github.com>
2021-02-04 20:39:08 -0300

    Exercícios de funções

--- Seção05_Funções/Exercícios/1.js
+++ Seção05_Funções/Exercícios/1.js
@@ -1,3 +1,6 @@
+/*Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.*/
+
 const operacoes = function (n1, n2) {
     console.log('Soma: ' + (n1 + n2)
         + '\nSubtração: ' + (n1 - n2)
---
+++ Seção05_Funções/Exercícios/2.js
@@ -0,0 +1,26 @@
+/*Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+Equilátero: Os três lados são iguais. Isósceles: Dois lados iguais. Escaleno: Todos os lados são diferentes.
+Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto
+ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
+triângulo).
+*/
+
+const triangulo = (L1, L2, L3) => {
+    if (L1 == L2 && L1 == L3) {
+        return console.log('Triângulo Equilátero!')
+    }
+    else if (L1 != L2 && L1 != L3 && L2 != L3) {
+        return console.log('Triângulo Escaleno!')
+    }
+    else {
+        return console.log('Triângulo Isósceles!')
+    }
+}
+
+triangulo(1, 2, 3)
+
+triangulo(1, 1, 3)
+
+triangulo(1, 1, 1)
+
+triangulo(1, 3, 3)
